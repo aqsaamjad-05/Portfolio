6089a8935e85e465af721bdc7c421f54c5440000
Aqsa Amjad <aqsa_amjad@outlook.com>
2024-09-21 20:42:26 +0100

    home page (not responsive)

--- js/script.js
+++ js/script.js
@@ -1,12 +1,9 @@
 let navItems = document.getElementById("nav-items");
 
-// Initially hide the menu
-navItems.style.maxHeight = "0px";
-
 function toggleMenu() {
     if (navItems.style.maxHeight === "0px" || navItems.style.maxHeight === "") {
-        navItems.style.maxHeight = "300px"; // Expand to show the menu
+        navItems.style.maxHeight = "300px"; // expand to show the menu
     } else {
-        navItems.style.maxHeight = "0px"; // Collapse to hide the menu
+        navItems.style.maxHeight = "0px"; // collapse to hide the menu
     }
 }
